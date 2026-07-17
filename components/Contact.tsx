2b10ad2da52a8010432ac4268d1fdd58caf010ba
george-ghionea <george.ghionea@consultant.com>
2026-07-17 19:33:52 +0300

    File update

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -29,7 +29,7 @@ const Contact: React.FC = () => {
               <svg className="w-6 h-6 text-slate-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
             </div>
             <h3 className="font-bold text-slate-900 mb-2">Office</h3>
-            <p className="text-slate-600 font-medium">Bucharest, Romania</p>
+            <p className="text-slate-600 font-medium">USA / Romania</p>
           </div>
         </div>
       </div>
